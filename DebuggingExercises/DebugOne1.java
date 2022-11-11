b5e25f5a51222fc6253d0c6b048a76df5ac60244
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-11-11 16:38:29 +0800

    Fix debugging issue.

--- DebuggingExercises/DebugOne1.java
+++ DebuggingExercises/DebugOne1.java
@@ -1,7 +1,9 @@
-public class DebugOne1
-
+public class FixDebugOne1     // change DebugOne1 to FixDebugOne1
+// add missing { before code
+{
    /* This program displays a greeting */
    public static void main(String[] args)
    {
-      Systemoutprintl("Hello).
+      Systemoutprintl("Hello");  // add missing semicolon after greeting
    }
+}
--- DebuggingExercises/DebugOne3.java
+++ DebuggingExercises/DebugOne3.java
@@ -1,9 +1,9 @@
-public class DebugOne33
+public class FixDebugOne33 // change DebugOne33 to FixDebugOne33
 {
    public static void main(String[] args)
    {
-      System.Out.println("Over the river");
-      system.out.println("and through the woods");
-      SysTem.0ut.println("to Grandmother's house we go");
+      System.out.println("Over the river");
+      System.out.println("and through the woods");          // inconsistency in capitalization in word "System"
+      SysTem.out.println("to Grandmother's house we go");   // o in .out is typed wrongly as .0 (zero)
    }
 }
