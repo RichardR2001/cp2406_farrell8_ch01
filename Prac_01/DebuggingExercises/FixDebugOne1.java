6c89485d1a581332bcbad6a5d154c2c40e4cf6f8
RichardR2001 <87416835+RichardR2001@users.noreply.github.com>
2022-11-18 12:20:46 +0800

    Add files via upload.

---
+++ Prac_01/DebuggingExercises/FixDebugOne1.java
@@ -0,0 +1,9 @@
+public class FixDebugOne1     // change DebugOne1 to FixDebugOne1
+// add missing { before code
+{
+   /* This program displays a greeting */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello");  // add missing semicolon after greeting
+   }
+}
---
+++ Prac_01/DebuggingExercises/FixDebugOne3.java
@@ -0,0 +1,9 @@
+public class FixDebugOne3 // change DebugOne33 to FixDebugOne3
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Over the river");
+      System.out.println("and through the woods");          // inconsistency in capitalization in word "System"
+      System.out.println("to Grandmother's house we go");   // o in .out is typed wrongly as .0 (zero)
+   }
+}
