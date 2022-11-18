6e77baa691c940f1d9a4d2185fa4a08ce93b0831
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-11-18 13:10:09 +0800

    Initial commit.

---
+++ Prac_01/Exercises/FavoriteSong.java
@@ -0,0 +1,14 @@
+// FavoriteSong.java
+// Chapter 1, Exercise #8
+// Displays lines of favorite song
+
+class FavoriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("So when it's late at night and you're still wide awake"); //Display the string
+        System.out.println("If you're with your friends or you're lonely");
+        System.out.println("When you hear somebody start to say my name");
+        System.out.println("Oh, when you think of me, I hope you think good things");
+    }
+}
