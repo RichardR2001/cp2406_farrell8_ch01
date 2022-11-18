7070e0eb1dee9441b2001bb54901511a25376b2a
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-11-18 13:04:44 +0800

    Initial commit.

---
+++ Prac_01/Exercises/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+// MovieQuoteInfo.java
+// Chapter 1, Exercise #7
+// Displays a movie quote and the details in which the quote come from such as the character that says it, the movie details.
+
+class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I'll be back.");
+        System.out.println("Arnold Schwarzenegger");
+        System.out.println("Terminator");
+        System.out.println("1984");
+    }
+}
