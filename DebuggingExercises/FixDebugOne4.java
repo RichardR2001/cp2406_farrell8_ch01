3335a923cb2eea8a70653297d2965088b0cec19d
Richard Reynard <richard.reynard@my.jcu.edu.au>
2022-11-18 12:18:39 +0800

    Fix path issue.

--- DebuggingExercises/FixDebugOne4.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class FixDebugOne4  // change DebugOne4 to FixDebugOne4
-{
-  public static void main(String[] args)    // missing void
-  {
-     JOptionPane.showMessageDialog(null, "First GUI program");  // missing inverted commas and wrong sign used (! instead of ;)
-  }
-}
